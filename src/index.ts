b78848dcc90193d794c453748b8345fa8f98c85b
Simon Cozens <simon@simon-cozens.org>
2025-12-16 11:40:59 +0000

    Docs and formatting nits

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 /**
- * {@include ../README.md}
+ * This is `fonttypes`, a TypeScript library for font engineering.
+ * It will be primarily useful for people working with variable fonts,
+ * especially at the source level; it includes types for designspaces,
+ * coordinates, locations, variation models, and provides utility functions
+ * for OpenType interpolation.
  * @packageDocumentation
  *
  * @groupDescription Coordinates And Locations
